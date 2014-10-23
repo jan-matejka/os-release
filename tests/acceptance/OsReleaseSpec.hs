6859055c1e12b974be1243ec1db6b1ee00a2816e
Jan (yac) Matějka <JMatejka@suse.cz>
2014-10-23 17:58:15 +0200

    Add machines to run acceptance on

--- tests/acceptance/OsReleaseSpec.hs
+++ tests/acceptance/OsReleaseSpec.hs
@@ -6,14 +6,15 @@ import Prelude hiding (EQ, LT, GT)
 import System.OsRelease
 import System.Environment.XDG.BaseDir
 import Data.Maybe
+import System.FilePath.Posix
 
 type OsId = String
 
 spec :: Spec
 spec = do
-    describe "nmap" $ do
-        it "works" $ do
-            cg <- getUserConfigFile "os-release" "acceptance.cf"
+    describe "detect" $ do
+        it "detects" $ do
+            cg <- getUserConfigFile "os-release" ("acceptance" </> "detect-os")
             f  <- readFile cg
             let osid = fromJust . readOsId $ lines f
 
