c4b4a1ef1f09d4417d9266fd976dcc97ebdfc920
yac <yac@blesmrt.net>
2015-01-31 21:00:30 +0100

    Add basic os-release parser

--- library/System/OsRelease.hs
+++ library/System/OsRelease.hs
@@ -2,11 +2,56 @@ module System.OsRelease
     ( detect
     , OsRelease (..)
     , detect'
+    , parseOs
+    , readOs
     )
 where
 
+import Text.ParserCombinators.Parsec
+-- import Data.Map
+
 data OsRelease = OpenSUSE
     deriving (Eq, Show)
+
+-- type OsReleaseRaw = Map String String
+type OsReleaseRaw = [(String, String)]
+
+osReleaseParser :: Parser [(String, String)]
+osReleaseParser = sepEndBy line eol
+  where
+    line :: Parser (String, String)
+    line  = do
+        var' <- var
+        _ <- char '='
+        val' <- val
+        return (var', val')
+
+    var :: Parser String
+    var   = many1 alphaNum
+
+    val :: Parser String
+    val   = qVal <|> nqVal
+
+    qVal :: Parser String
+    qVal  = do
+        quote <- oneOf "'\""
+        value <- nqVal
+        _ <- char quote
+        return value
+
+    nqVal :: Parser String
+    nqVal = many1 alphaNum
+
+    eol :: Parser Char
+    eol   = char '\n'
+
+readOs :: IO (Either ParseError OsReleaseRaw)
+readOs = do
+    xs <- readFile "/etc/os-release"
+    return $ parseOs xs
+
+parseOs :: String -> Either ParseError OsReleaseRaw
+parseOs xs = parse osReleaseParser "os-release" xs
 
 detect :: IO (Maybe OsRelease)
 detect = do
--- tests/unit/OsReleaseSpec.hs
+++ tests/unit/OsReleaseSpec.hs
@@ -11,3 +11,28 @@ spec = do
             detect' []                     `shouldBe` Nothing
             detect' ["NAME=open"]          `shouldBe` Nothing
             detect' ["NAME=\"openSUSE\""]  `shouldBe` Nothing
+
+    describe "parseOs" $ do
+        it "parses simple value"                   $ fooBar "foo=bar"
+        it "parses simple value with trailing NL"  $ fooBar "foo=bar\n"
+        it "parses single quoted value"            $ fooBar "foo='bar'"
+        it "parses double quoted value"            $ fooBar "foo=\"bar\""
+        it "parses multiple values" $ parseCase
+            "foo=bar\nqux=quux"
+            [("foo", "bar"), ("qux", "quux")]
+
+        it "breaks on misquoting 1" $ errCase "foo=\"bar'"
+        it "breaks on misquoting 2" $ errCase "foo='bar\""
+
+    where
+        fooBar x = parseCase x [("foo", "bar")]
+
+        parseCase x y =
+            case parseOs x of
+                Left  e -> expectationFailure $ show e
+                Right z -> z `shouldBe` y
+
+        errCase x =
+            case parseOs x of
+                Left  _ -> "1" `shouldBe` "1"
+                Right z -> expectationFailure (show z)
