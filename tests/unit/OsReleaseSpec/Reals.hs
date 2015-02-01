3f40d8bbce6256fe448843151ff31fcc853138bc
yac <yac@blesmrt.net>
2015-02-02 00:09:05 +0100

    omg so pretty

--- tests/unit/OsReleaseSpec/Reals.hs
+++ tests/unit/OsReleaseSpec/Reals.hs
@@ -26,20 +26,21 @@ data Reals = Gentoo | OpenSUSEFactory | Fedora20 | Arch
 
 testVals :: Reals -> [(TestVal, TestVal)]
 testVals Gentoo =
-    [ (Var "NAME"           , Val "Gentoo")
-    , (Var "ID"             , Val "gentoo")
+    [ (Var "NAME"           , Val  "Gentoo")
+    , (Var "ID"             , Val  "gentoo")
     , (Var "PRETTY_NAME"    , QVal "Gentoo/Linux")
     , (Var "ANSI_COLOR"     , QVal "1;32")
     , (Var "HOME_URL"       , QVal "http://www.gentoo.org/")
     , (Var "SUPPORT_URL"    , QVal "http://www.gentoo.org/main/en/support.xml")
     , (Var "BUG_REPORT_URL" , QVal "https://bugs.gentoo.org/")
     ]
+
 testVals OpenSUSEFactory =
-    [ (Var "NAME"           , Val "openSUSE")
+    [ (Var "NAME"           , Val  "openSUSE")
     , (Var "VERSION"        , QVal "20150115 (Tumbleweed)")
     , (Var "VERSION_ID"     , QVal "20150115")
     , (Var "PRETTY_NAME"    , QVal "openSUSE 20150115 (Tumbleweed) (x86_64)")
-    , (Var "ID"             , Val "opensuse")
+    , (Var "ID"             , Val  "opensuse")
     , (Var "ANSI_COLOR"     , QVal "0;32")
     , (Var "CPE_NAME"       , QVal "cpe:/o:opensuse:opensuse:20150115")
     , (Var "BUG_REPORT_URL" , QVal "https://bugs.opensuse.org")
@@ -48,19 +49,19 @@ testVals OpenSUSEFactory =
     ]
 
 testVals Fedora20 =
-    [ (Var "NAME"                           , Val "Fedora")
+    [ (Var "NAME"                           , Val  "Fedora")
     , (Var "VERSION"                        , QVal "20 (Heisenbug)")
-    , (Var "ID"                             , Val "fedora")
-    , (Var "VERSION_ID"                     , Val "20")
+    , (Var "ID"                             , Val  "fedora")
+    , (Var "VERSION_ID"                     , Val  "20")
     , (Var "PRETTY_NAME"                    , QVal "Fedora 20 (Heisenbug)")
     , (Var "ANSI_COLOR"                     , QVal "0;34")
     , (Var "CPE_NAME"                       , QVal "cpe:/o:fedoraproject:fedora:20")
     , (Var "HOME_URL"                       , QVal "https://fedoraproject.org/")
     , (Var "BUG_REPORT_URL"                 , QVal "https://bugzilla.redhat.com/")
     , (Var "REDHAT_BUGZILLA_PRODUCT"        , QVal "Fedora")
-    , (Var "REDHAT_BUGZILLA_PRODUCT_VERSION", Val "20")
+    , (Var "REDHAT_BUGZILLA_PRODUCT_VERSION", Val  "20")
     , (Var "REDHAT_SUPPORT_PRODUCT"         , QVal "Fedora")
-    , (Var "REDHAT_SUPPORT_PRODUCT_VERSION" , Val "20")
+    , (Var "REDHAT_SUPPORT_PRODUCT_VERSION" , Val  "20")
     ]
 
 testVals Arch =
